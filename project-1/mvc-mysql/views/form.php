055d6e29cbb1ed53cc0b6beaf7159eb08c9fcd9e
whoisjuarez <133733073+whoisjuarez@users.noreply.github.com>
2024-01-10 21:30:01 -0500

    Organized project folder

--- project-1/mvc-mysql/views/form.php
+++
@@ -1,6 +0,0 @@
-<form method="POST">
-    <input type="text" name="speciesName" placeholder="Name of the Species" value="<?php echo (isset($_POST['speciesName']) ? $_POST['speciesName'] : ""); ?>">
-    
-    <input type="submit" name="submit" value="Submit">
-    <input type="reset" name="reset" value="Reset">
-</form>
